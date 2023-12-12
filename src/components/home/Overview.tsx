57e1ec77b7b810f312839d478364eacea9859dab
Abhirami <abhiramigs07@gmail.com>
2023-12-12 13:45:57 +0530

    fix🛠️:Home page changes

--- src/components/home/Overview.tsx
+++ src/components/home/Overview.tsx
@@ -1,3 +1,82 @@
+// "use client"
+
+// import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+
+// const data = [
+//   {
+//     name: "Jan",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Feb",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Mar",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Apr",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "May",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Jun",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Jul",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Aug",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Sep",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Oct",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Nov",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+//   {
+//     name: "Dec",
+//     total: Math.floor(Math.random() * 5000) + 1000,
+//   },
+// ]
+
+// export function Overview() {
+//   return (
+//     <ResponsiveContainer width="100%" height={350}>
+//       <BarChart data={data}>
+//         <XAxis
+//           dataKey="name"
+//           stroke="#888888"
+//           fontSize={12}
+//           tickLine={false}
+//           axisLine={false}
+//         />
+//         <YAxis
+//           stroke="#888888"
+//           fontSize={12}
+//           tickLine={false}
+//           axisLine={false}
+//           tickFormatter={(value) => `$${value}`}
+//         />
+//         <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
+//       </BarChart>
+//     </ResponsiveContainer>
+//   )
+// }
+
 "use client"
 import React from 'react';
 import {
@@ -171,12 +250,12 @@ const data = [
   },
 ];
 
-const HomeChart = () => {
+const Overview = () => {
  
     return (
       
       
-        <LineChart width={300} height={500} data={data}>
+        <LineChart width={900} height={400} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
           <YAxis />
@@ -190,5 +269,5 @@ const HomeChart = () => {
   
 }
 
-export default HomeChart;
+export default Overview;
 
