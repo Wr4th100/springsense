6f20fa8db52510e0c2f443d0a90d76ddf5999c5d
Roshan Jagadish <roshan10.rj@gmail.com>
2023-12-21 19:39:33 +0530

    fix: Removed seed comments

--- prisma/seeds/spring.ts
+++ prisma/seeds/spring.ts
@@ -1,33 +1,33 @@
 import { type PrismaClient } from "@prisma/client";
 
 export default async function seedSprings(prisma: PrismaClient) {
-  //   const azhagar = await prisma.springShed.create({
-  //     data: {
-  //       name: "Alagar hills springshed",
-  //       state: "Tamil Nadu",
-  //     },
-  //   });
+  const azhagar = await prisma.springShed.create({
+    data: {
+      name: "Alagar hills springshed",
+      state: "Tamil Nadu",
+    },
+  });
 
-  //   const nagamalai = await prisma.springShed.create({
-  //     data: {
-  //       name: "Nagamalai Springshed",
-  //       state: "Tamil Nadu",
-  //     },
-  //   });
+  const nagamalai = await prisma.springShed.create({
+    data: {
+      name: "Nagamalai Springshed",
+      state: "Tamil Nadu",
+    },
+  });
 
-  //   const kodaikanal = await prisma.springShed.create({
-  //     data: {
-  //       name: "Kodaikanal springshed",
-  //       state: "Tamil Nadu",
-  //     },
-  //   });
+  const kodaikanal = await prisma.springShed.create({
+    data: {
+      name: "Kodaikanal springshed",
+      state: "Tamil Nadu",
+    },
+  });
 
-  //   const nilgiris = await prisma.springShed.create({
-  //     data: {
-  //       name: "Nilgiris Springshed",
-  //       state: "Tamil Nadu",
-  //     },
-  //   });
+  const nilgiris = await prisma.springShed.create({
+    data: {
+      name: "Nilgiris Springshed",
+      state: "Tamil Nadu",
+    },
+  });
 
   const springshed1 = await prisma.springShed.create({
     data: {
@@ -36,28 +36,26 @@ export default async function seedSprings(prisma: PrismaClient) {
     },
   });
 
-    const springshed2 = await prisma.springShed.create({
-        data: {
-        name: "Springshed 2",
-        state: "Uttarakhand",
-        },
-    });
+  const springshed2 = await prisma.springShed.create({
+    data: {
+      name: "Springshed 2",
+      state: "Uttarakhand",
+    },
+  });
 
-    const springshed3 = await prisma.springShed.create({
-        data: {
-        name: "Springshed 3",
-        state: "Odisha",
-        },
-    });
+  const springshed3 = await prisma.springShed.create({
+    data: {
+      name: "Springshed 3",
+      state: "Odisha",
+    },
+  });
 
-    const springshed4 = await prisma.springShed.create({
-        data: {
-        name: "Springshed 4",
-        state: "Odisha",
-        },
-    });
+  const springshed4 = await prisma.springShed.create({
+    data: {
+      name: "Springshed 4",
+      state: "Odisha",
+    },
+  });
 
   console.log("Seeded springsheds");
 }
-
-
